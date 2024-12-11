ceb37d803844560b70fdf0accf7a9ba32a3fbc83
Kensaku-Hatori <241316@st.yoshida-g.ac.jp>
2024-12-11 10:09:02 +0900

    a

--- Run & Jump/Assets/SCRIPT/UnityChanController.cs
+++ Run & Jump/Assets/SCRIPT/UnityChanController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class UnityChanController : MonoBehaviour
 {
+    public float WallkSpeed = 1;
     public Rigidbody UnityChan;
     // Start is called before the first frame update
     void Start()
@@ -14,13 +15,14 @@ public class UnityChanController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && transform.position.x >= 0)
         {
             transform.Translate(-3, 0, 0);
         }
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        else if (Input.GetKeyDown(KeyCode.RightArrow) && transform.position.x <= 3)
         {
             transform.Translate(3, 0, 0);
         }
+        transform.Translate(0, 0, WallkSpeed / 10);
     }
 }
--- Run & Jump/Assets/UnityChan/unity-chan!/Unity-chan! Model/Scripts/UnityChanControlScriptWithRgidBody.cs
+++ Run & Jump/Assets/UnityChan/unity-chan!/Unity-chan! Model/Scripts/UnityChanControlScriptWithRgidBody.cs
@@ -106,8 +106,8 @@ namespace UnityChan
 			// 上下のキー入力でキャラクターを移動させる
 			transform.localPosition += velocity * Time.fixedDeltaTime;
 
-			// 左右のキー入力でキャラクタをY軸で旋回させる
-			transform.Rotate (0, h * rotateSpeed, 0);	
+			//// 左右のキー入力でキャラクタをY軸で旋回させる
+			//transform.Rotate (0, h * rotateSpeed, 0);	
 	
 
 			// 以下、Animatorの各ステート中での処理
