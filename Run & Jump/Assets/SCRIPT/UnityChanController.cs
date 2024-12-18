8fee209b4773bbbbe46a4d08720aa541e40bb37f
大野莉緒 <241302@st.yoshida-g.ac.jp>
2024-12-18 11:07:38 +0900

    アイテム取得とスコア

--- Run & Jump/Assets/SCRIPT/UnityChanController.cs
+++ Run & Jump/Assets/SCRIPT/UnityChanController.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;//LoadSceneを使うために必要
 
 public class UnityChanController : MonoBehaviour
 {
     public float WallkSpeed = 1;
     public Rigidbody UnityChan;
+    public GameObject ScoreText;//スコアのUI
+    public GameObject Item;
+    private int score;//スコア
+
     // Start is called before the first frame update
     void Start()
     {
         UnityChan = GetComponent<Rigidbody>();
+        score = 0;//得点の初期化
     }
 
     // Update is called once per frame
@@ -26,4 +33,21 @@ public class UnityChanController : MonoBehaviour
         }
         UnityChan.transform.Translate(0, 0, WallkSpeed / 10);
     }
+    private void OnTriggerEnter(Collider other)
+    {
+           //その収集アイテムを非表示にする
+           other.gameObject.SetActive(false);
+           score = score + 1;//スコア加算
+           SetText();
+
+    }
+    void SetText()
+    {
+        score *= 100;
+
+        //スコアの表示を更新
+        ScoreText.GetComponent<TextMeshProUGUI>().text = "Count:" + score.ToString();
+    }
+
+
 }
